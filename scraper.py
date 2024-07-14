29d4d88b5d188d9e97457049085890916109c567
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-07-14 16:47:21 +0200

    Sync

--- scraper.py
+++ scraper.py
@@ -19,11 +19,14 @@ wochentag = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
 def uhrzeit(datetime: datetime = datetime.now()) -> str:
     return (datetime - timedelta(hours=zeitdiff)).strftime("%H:%M")
 
+def datum(datetime: datetime = datetime.now()) -> str:
+    return (datetime - timedelta(hours=zeitdiff)).strftime("%d.%m.%Y")
+
 def loghead(msg: str):
     print(f"╔════════════════════════════════════════════════════════════════════")
     print(f"║ {msg}")
     print(f"╚═╦══════════════════════════════════════════════════════════════════")
-    print(f"  ║ Aktuelle Uhrzeit: {uhrzeit()}")
+    print(f"  ║ Aktuelle Uhrzeit: {uhrzeit()} am {datum()}")
     print(f"  ║ ")
 
 def log(msg: str):
@@ -57,19 +60,19 @@ def postToWebhook(msg: str):
         #print(f"Fehler beim Senden der Nachricht: {err}")
         ...
 
-def scrape(datum = date.today() - timedelta(days=1)):
-    loghead(f"Scrape-Versuch für den {datum.strftime("%d.%m.%Y")} begonnen")
+def scrape(date = date.today() - timedelta(days=1)):
+    loghead(f"[INFO] Scrape-Versuch für den {datum(date)} begonnen")
     try:
-        gestern = vp.fetch(datum)
-
+        tag = vp.fetch(date)
+        log(f"\033[32m[SUCCES] Daten vom {date} erfolgreich abgerufen\033[0m")
         try:
-            dateipfad = f"./data/{gestern.datum.strftime("%d.%m.%Y")} ({wochentag[gestern.datum.weekday()]})"
-            gestern.saveasfile(pfad=dateipfad, allowoverwrite=False)
-        except FileExistsError:
-            ...
+            dateipfad = f"./data/{tag.datum.strftime("%d.%m.%Y")} ({wochentag[tag.datum.weekday()]})"
+            tag.saveasfile(pfad=dateipfad, allowoverwrite=False)
+        except FileExistsError as e:
+            log(f"\033[31m[ERROR] Datei mit Pfad {dateipfad} existiert bereits \033[0m")
 
     except VpMobil.FetchingError:
-        ...
+        log(f"\033[31m[ERROR] Datei mit Pfad {dateipfad} existiert bereits \033[0m")
 
 # ╭──────────────────────────────────────────────────────────────────────────────────────────╮
 # │                                     Hauptprogramm                                        │ 
--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-import datetime
-
-datum = datetime.date(2024, 6, 19)
-
-print(datum.strftime("%d.%m.%Y"), datum.weekday())
+╔════════════════════════════════════════════════════════════════════
+║ Scrape-Versuch für den 19.06.2024 begonnen
+╚═╦══════════════════════════════════════════════════════════════════
+  ║ Aktuelle Uhrzeit: 16:35 am 14.07.2024
+  ║ 
+  ║ [ERROR] Das geht nicht
+  ║
+  ║
+  ║
+  ║
+  ║
