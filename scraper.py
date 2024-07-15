c6680dcf1aab2e264375381bbd0bcf4aa26fa5c2
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-07-15 12:08:26 +0200

    Sync

--- scraper.py
+++ scraper.py
@@ -15,6 +15,7 @@ load_dotenv()
 # ╰──────────────────────────────────────────────────────────────────────────────────────────╯
 
 zeitdiff = int(os.getenv("TIME_DIFF"))
+SYSTEM = os.getenv("SYSTEM")
 
 wochentag = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
 
@@ -71,15 +72,14 @@ if os.path.exists("./data/latest.xml"):
 #         #print(f"Fehler beim Senden der Nachricht: {err}")
 #         ...
 
-def uploadToGitHub(dateipfad):
+def uploadToGitHub(datei, zielpfad):
 
-    dateiname = os.path.basename(dateipfad)
-    url = f'https://api.github.com/repos/annhilati/vertretungsplan-stats/contents/data/{dateiname}'
+    url = f'https://api.github.com/repos/annhilati/vertretungsplan-stats/contents/{zielpfad}'
 
 
-    with open(dateipfad, 'rb') as datei:
-        inhalt = datei.read()
-    base64_content = base64.b64encode(inhalt).decode('utf-8')
+    with open(datei, 'rb') as f:
+        content = f.read()
+    content_base64 = base64.b64encode(content).decode('utf-8')
 
     headers = {
         'Authorization': f'token {GITHUB_TOKEN}',
@@ -87,38 +87,42 @@ def uploadToGitHub(dateipfad):
     }
     data = {
         'message': "Vertretungsplan-Scraper-Upload",
-        'content': base64_content
+        'content': content_base64
     }
     
     response = requests.put(url, json=data, headers=headers)
     
     if response.status_code == 201:
-        log(f"\033[32m[SUCCES] Datei \"{dateiname}\" erfolgreich hochgeladen\033[0m")
+        log(f"\033[32m[SUCCES] Datei \"{zielpfad}\" erfolgreich hochgeladen\033[0m")
     else:
-        log(f'\033[31m[ERROR] Datei \"{dateiname}\" konnte nicht hochgeladen werden: {response.status_code}\033[0m')
+        log(f'\033[38;2;255;165;0m[WARN] Datei \"{zielpfad}\" konnte nicht hochgeladen werden: {response.status_code}\033[0m')
+        if response.status_code == 422:
+            log("   [INFO] Die Datei wurde nicht hochgeladen, da sie bereits mit exakt dem selben Inhalt existiert.")
         #print(response.json())
 
 def scrape(date = date.today() - timedelta(days=1)):
     loghead(f"Scrape-Versuch für den {datum(date)} begonnen")
 
-    dateipfad = f"./data/{date.strftime("%Y-%m-%d")} ({wochentag[date.weekday()]}).xml"
+    dateiname = f"{date.strftime("%Y-%m-%d")} ({wochentag[date.weekday()]}).xml"
+    datenverzeichnis = f"./data" if SYSTEM == "live" else "./test"
+    zieldateipfad = f"{datenverzeichnis}/{dateiname}"
 
     try:
         tag = vp.fetch(date)
         log(f"\033[32m[SUCCES] Daten vom {datum(date)} erfolgreich abgerufen\033[0m")
 
         try:
-            tag.saveasfile(pfad=dateipfad, allowoverwrite=False)
+            tag.saveasfile(pfad=zieldateipfad, allowoverwrite=False)
  
-            tag.saveasfile(pfad=f"./data/latest.xml", allowoverwrite=True)
+            tag.saveasfile(pfad=f"{datenverzeichnis}/latest.xml", allowoverwrite=True)
 
-            log(f"\033[32m[SUCCES] Dateien wurden in data/ angelegt\033[0m")
+            log(f"\033[32m[SUCCES] Dateien wurden in {datenverzeichnis}/ angelegt\033[0m")
 
-            uploadToGitHub(dateipfad)
+            uploadToGitHub(datei=zieldateipfad, zielpfad=zieldateipfad)
 
         except FileExistsError:
-            log(f"\033[38;2;255;165;0m[CONFLICT] Datei mit Pfad \"{dateipfad}\" existiert bereits \033[0m")
-            log(f"-> Anlegung und Upload neuer Dateien wird übersprungen")
+            log(f"\033[38;2;255;165;0m[CONFLICT] Datei mit Pfad \"{zieldateipfad}\" existiert bereits \033[0m")
+            log(f"  -> Anlegung und Upload neuer Dateien wird übersprungen")
 
     except VpMobil.FetchingError:
         if wochentag[date.weekday()] not in ["Sa", "So"]:
@@ -126,18 +130,18 @@ def scrape(date = date.today() - timedelta(days=1)):
             
             if date not in freieTage:
                 log(f"\033[31m[ERROR] Daten vom {datum(date)} konnten nicht abgerufen werden \033[0m")
-                log(f"-> Eine Platzhalterdatei wird erstellt und hochgeladen")
-                log(f"-> Versende Benachrichtung an Webhook")
+                log(f"  -> Eine Platzhalterdatei wird erstellt und hochgeladen")
+                log(f"  -> Versende Benachrichtung an Webhook")
 
-                with open(f"{dateipfad}.ERROR", "w") as f: pass
-                uploadToGitHub(f"{dateipfad}.ERROR")
+                with open(f"{zieldateipfad}.ERROR", "w") as f: pass
+                uploadToGitHub(f"{zieldateipfad}.ERROR")
             
             elif date in freieTage:
                 log(f"[INFO] Daten vom {datum(date)} wurden nicht abgerufen (als frei markierter Tag)")
-                log(f"-> Eine Platzhalterdatei wird erstellt und hochgeladen")
+                log(f"  -> Eine Platzhalterdatei wird erstellt und hochgeladen")
 
-                with open(f"{dateipfad}.frei", "w") as f: pass
-                uploadToGitHub(f"{dateipfad}.frei")
+                with open(f"{zieldateipfad}.frei", "w") as f: pass
+                uploadToGitHub(f"{zieldateipfad}.frei")
         
         elif wochentag[date.weekday()] in ["Sa", "So"]:
             log(f"[INFO] Daten vom {datum(date)} wurden nicht abgerufen (Wochenende)")
