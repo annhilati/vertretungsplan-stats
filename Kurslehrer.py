5206e080961b52d858efc9eeb9bb8f3d8a617343
Annhilati <antbe.artis@web.de>
2024-11-18 18:05:06 +0100

    Create Kurslehrer.py

---
+++ Kurslehrer.py
@@ -0,0 +1,17 @@
+from vpmobil import Vertretungsplan, VpDay
+from datetime import datetime, timedelta
+
+vp = Vertretungsplan(schulnummer=10126582,
+                     benutzername="schueler",
+                     passwort="s361o97")
+
+daten: list[VpDay] = []
+for tag in range(20241106, 20241122):
+    fetch = vp.fetch(datei=f"10126582/mobil/mobdaten/PlanKl{tag}.xml")
+    daten.append(fetch)
+
+for tag in daten:
+    for klasse in tag.klassen():
+        if klasse.kürzel == 11 or 12:
+            for kurs in klasse.alleKurse():
+                print(f"{kurs.fach} | {kurs.lehrer}")
